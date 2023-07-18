dda04526ccf5971eb2d099e519dea6dc94e0ec4c
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-18 19:16:49 +0100

    Convertir la actividad 4 en función

--- actividad4.js
+++ actividad4.js
@@ -15,14 +15,27 @@ var ninas = parseInt(prompt("Números de niñas: "));
 
 console.log ("Niños: "+ ninos);
 console.log ("Niñas: "+ ninas);
-
-var totalninos = ninos + ninas;
-var porcentninos = (ninos*100)/ totalninos;
-var porcentninas = (ninas*100)/ totalninos;
-
-console.log("El porcentaje de niñas es: " + porcentninas + " y el porcentaje de niños es: " + porcentninos);
+document.write("Niños: "+ ninos + "<br>");
+document.write("Niñas: "+ ninas+ "<br>");
 
 
+// var totalninos = ninos + ninas;
+// var porcentninos = (ninos*100)/ totalninos;
+// var porcentninas = (ninas*100)/ totalninos;
+
+// console.log("El porcentaje de niñas es: " + porcentninas + " y el porcentaje de niños es: " + porcentninos);
+// document.write("El porcentaje de niñas es: " + porcentninas + " y el porcentaje de niños es: " + porcentninos);
+
+function contar(){
+    var totalninos = ninos + ninas;
+    var porcentninos = (ninos*100)/ totalninos;
+    var porcentninas = (ninas*100)/ totalninos;
+
+    console.log("El porcentaje de niñas es: " + porcentninas + " y el porcentaje de niños es: " + porcentninos);
+    document.write("El porcentaje de niñas es: " + porcentninas + " y el porcentaje de niños es: " + porcentninos+ "<br>");
+}
+
+contar();
 
 
 
