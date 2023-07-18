408d9b15dc7eb1ffeb298311bf11f0f16ec58800
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-18 19:11:36 +0100

    Cambiar la actividad 3 a una función

--- actividad3.js
+++ actividad3.js
@@ -25,10 +25,25 @@ var num2 = parseInt(prompt("Segundo número"));
 console.log("El primer número es: "+ num1);
 console.log("El segundo número es: " + num2);
 
-if(num1 == num2){
-    console.log(num1 + " y " + num2 + " son iguales" )
-} else if( num1 > num2){
-    console.log(num1 + " es mayor que " + num2 )
-}else{
-    console.log(num1 + " es menor que " + num2 )
+// if(num1 == num2){
+//     console.log(num1 + " y " + num2 + " son iguales" )
+// } else if( num1 > num2){
+//     console.log(num1 + " es mayor que " + num2 )
+// }else{
+//     console.log(num1 + " es menor que " + num2 )
+// }
+
+function compara(){
+    if(num1 == num2){
+        console.log(num1 + " y " + num2 + " son iguales" );
+        document.write(num1 + " y " + num2 + " son iguales" );
+    } else if( num1 > num2){
+        console.log(num1 + " es mayor que " + num2 );
+        document.write(num1 + " es mayor que " + num2 );
+    }else{
+        console.log(num1 + " es menor que " + num2 );
+        document.write(num1 + " es menor que " + num2 );
+    }
 }
+
+compara();
