bca64e060ddd1284d6f4e2720e3b90ef6e153559
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-18 20:03:53 +0100

    Siguiente intento del ejercicio 6

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,31 +1,43 @@
 // Solicitar clave
-// Si es correcta decir clave correcta
+// Si es correcta, decir clave correcta
 // Sino volver a solicitar introducir la clave
-// Si es correcta decir clave correcta
+// Si es correcta, decir clave correcta
 // Sino volver a solicitar introducir la clave
-// Si es correcta decir clave correcta
+// Si es correcta, decir clave correcta
 // Sino decir clave incorrecta no quedan más oportunidades
 
 
 
-
-
-
-
-function comparacion(){
-    var compara= parseInt(prompt("Introduce la clave: "));
-    for(var n= 3; n>0; n--){
+// function comparacion(){
+//     var compara= parseInt(prompt("Introduce la clave: "));
+//     for(var n= 3; n>0; n--){
         
-        if(compara == 123456){
-            document.write("Has introducido la clave correctamente" + "</br>");
-        }
-        else if(compara != 123456){
-            parseInt(prompt("No has introducido la clave correcta, vuelve a intentarlo"));
-        }
-        else{
-            document.write("Te has quedado sin intentos")
-        }
+//         if(compara == 123456){
+//             document.write("Has introducido la clave correctamente" + "</br>");
+//         }
+//         else if(compara != 123456){
+//             parseInt(prompt("No has introducido la clave correcta, vuelve a intentarlo"));
+//         }
+//         else{
+//             document.write("Te has quedado sin intentos")
+//         }
 
+//     }
+// }
+
+
+var compara= parseInt(prompt("Introduce la contraseña: "));
+function comparacion(){
+    if(compara==123456){
+        document.write("Has introducido la clave correctamente" + "</br>");
+        console.log("Has introducido la clave correctamente");
+    }
+    else if(compara!=123456){
+        parseInt(prompt("Introduce la contraseña: "));
+    }
+    else{
+        document.write("No has introducido la clave correctamente" + "</br>");
+        console.log("No has introducido la clave correctamente");
     }
 }
 
