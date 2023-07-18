45215d6bd78f03c99a0eb74b0898c40b4101b112
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-18 19:04:54 +0100

    Cambios en ejercicio 3 y 6

--- ejercicio6.js
+++ ejercicio6.js
@@ -6,23 +6,26 @@
 // Si es correcta decir clave correcta
 // Sino decir clave incorrecta no quedan más oportunidades
 
-var clave= 123456;
 
-var compara= parseInt(prompt("Introduce la clave: "));
+
+
 
 
 
 function comparacion(){
-    for(var n= 1; n<= 2; n++){
-        if(compara == clave){
+    var compara= parseInt(prompt("Introduce la clave: "));
+    for(var n= 3; n>0; n--){
+        
+        if(compara == 123456){
             document.write("Has introducido la clave correctamente" + "</br>");
         }
-        else if(compara != clave){
+        else if(compara != 123456){
             parseInt(prompt("No has introducido la clave correcta, vuelve a intentarlo"));
         }
-        if(compara != clave){
-            document.write("Te has quedado sin intentos" + "</br>");
+        else{
+            document.write("Te has quedado sin intentos")
         }
+
     }
 }
 
