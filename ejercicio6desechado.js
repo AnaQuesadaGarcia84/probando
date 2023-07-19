27cc87f8821419af46c9fa05cdd50d198d7f8643
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-19 19:17:31 +0100

    Codigo comentado del ejercicio 6 en otro documento

---
+++ ejercicio6desechado.js
@@ -0,0 +1,58 @@
+// for ( var i=0;i<=2;i++) { 
+//    	document.write(i + "<br>")
+// }
+
+// var compara = parseInt(prompt("Introduce la clave"));
+// if(compara==123456){
+//     document.write("Has introducido la clave correctamente" + "</br>");
+// }
+// else{
+//     parseInt(prompt("No has introducido la clave correcta, vuelve a intentarlo"));
+// }
+
+// var clave=123456 
+// var clave2=parseInt(prompt("Introduce la clave"));
+// if (clave == clave2){ 
+//    document.write("Has introducido la clave correctamente");
+// }
+// else if (clave > clave2) { 
+//     console.log("clave incorrecta 1")
+//     parseInt(prompt("Introduce la clave")); 
+// }
+// else{ 
+//     console.log("clave incorrecta 2")
+//     parseInt(prompt("Introduce la clave"));
+//     document.write("Clave incorrecta")
+// } 
+
+// function compara(){
+//     var clave2=parseInt(prompt("Introduce la clave"));
+//     if (clave == clave2){ 
+//     document.write("Has introducido la clave correctamente");
+//     }
+//     else if (clave > clave2) { 
+//         console.log("clave incorrecta 1")
+//         parseInt(prompt("Introduce la clave")); 
+//     }
+//     else{ 
+//         console.log("clave incorrecta 2")
+//         parseInt(prompt("Introduce la clave"));
+//         document.write("Clave incorrecta")
+//     } 
+// }
+
+
+// var compara= parseInt(prompt("Introduce la contraseña: "));
+// function comparacion(){
+//     if(compara==123456){
+//         document.write("Has introducido la clave correctamente" + "</br>");
+//         console.log("Has introducido la clave correctamente");
+//     }
+//     else if(compara!=123456){
+//         parseInt(prompt("Introduce la contraseña: "));
+//     }
+//     else{
+//         document.write("No has introducido la clave correctamente" + "</br>");
+//         console.log("No has introducido la clave correctamente");
+//     }
+// }
