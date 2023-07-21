7947f7ccb9ce8fd00c44ba55871898a3fc52cbc6
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-21 19:29:38 +0100

    Creación de nuevas variables

--- actividad2.js
+++ actividad2.js
@@ -1,13 +1,17 @@
-var a = document.getElementById("num1").value;
-var b = document.getElementById("num2").value;
+document.getElementById('comprueba').onclick=function operaciones(){
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
 
-function operaciones(){
-    document.getElementById("operandos").innerHTML="Primer número: " + a + " Segundo número: " + b+ "<br>";
-    document.write("Sumar: " + (a+b) + "<br>");
-    document.write("Restar: " + (a-b) + "<br>");
-    document.write("Multiplicación: " + (a*b) + "<br>");
-    document.write("Dividisión: " + (a/b) + "<br>");
-    document.write("<hr>");
+    var suma= num1+num2;
+    var resta= num1-num2;
+    var multiplica=num1*num2;
+    var divide=num1/num2;
+
+    document.getElementById('operandos').innerHTML= 'Primer número: '+num1+ ' Segundo número: '+num2;
+    document.getElementById('suma').innerHTML='El resultado al sumarlos es: '+suma + '<br>';
+    document.getElementById('suma').innerHTML='El resultado al restarlos es: '+resta+ '<br>';
+    document.getElementById('suma').innerHTML='El resultado al multiplicarlos es: '+multiplica+ '<br>';
+    document.getElementById('suma').innerHTML='El resultado al dividirlos es: '+divide+ '<br>';    
 }
 
 operaciones();
