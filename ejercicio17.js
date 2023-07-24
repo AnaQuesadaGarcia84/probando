1bfd98ee5541cf002ae93b303ae481cb26b30948
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-24 20:11:03 +0100

    Edición index2 y ejercicio 17 y creación de ejercicio 18 y index3

--- ejercicio17.js
+++ ejercicio17.js
@@ -16,7 +16,15 @@ console.log(separar[separar.length-1]);
 
 
 
-document.getElementById("escribir").innerHTML="<h2>Mi texto: "+ cadena+"</h2>"+"<h2>La cantidad de palabras es: "+separar.length+"</h2>"+
-"<h2>Primera pocisión del array: "+separar[0]+"</h2>"+"<h2>Última posición del array: "+separar[separar.length-1]+"</h2>";
+// document.getElementById("escribir").innerHTML="<h2>Mi texto: "+ cadena+"</h2>"+"<h2>La cantidad de palabras es: "+separar.length+"</h2>"+
+// "<h2>Primera pocisión del array: "+separar[0]+"</h2>"+"<h2>Última posición del array: "+separar[separar.length-1]+"</h2>";
 
-
+function pulsar(){
+    var arr=[];
+    arr=document.formulario.miarea.value.split(" ");
+    console.log(arr);
+    var n_Palabras=arr.length;
+    var p_Palabra=arr[0];
+    var u_Palabra=arr[arr.length-1];
+    document.getElementById("resultado").innerHTML="Número de palabras: " +n_Palabras+"<br> Primera palabra: "+p_Palabra+"<br> Última palabra: "+u_Palabra
+}
---
+++ ejercicio18.js
@@ -0,0 +1,30 @@
+// 1.Crear un documento html con un formulario para pedir los datos de los alumnos:
+//     a.Código
+//     b.Nombre y Apellidos
+//     c.Edad
+//     d.Correo electrónico
+//     e.Teléfono
+//     f.Un select para seleccionar un tema (HTML, CSS, JavaScript)
+// 2.Crear un script para ir rellenando un array con los datos de un alumno.
+// 3.Mostrar los datos del alumno en el documento html.
+
+
+document.getElementById('validar').onclick=function validar(){
+    var arrDatos=[];
+
+    //Recogemos los datos desde el formulario
+    var codigo=document.getElementById('codigo').value;
+    var nombre=document.getElementById('nombre').value;
+    var edad=document.getElementById('edad').value;
+    var mail=document.getElementById('mail').value;
+    var tel=document.getElementById('telefono').value;
+    var lenguaje=document.getElementById('lenguaje').value
+    
+    console.log(codigo, nombre,edad,mail,tel,lenguaje);
+
+    //Introducimos los datos en el array
+    arrDatos.push(codigo, nombre,edad,mail,tel,lenguaje);
+
+    document.getElementById("mostrar").innerHTML= arrDatos;
+    // alert(arrDatos);
+}
