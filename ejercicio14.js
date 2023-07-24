6cb7c2c6dfdba358bbc79e317f5b73ac52f3fc6e
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-07-24 16:26:04 +0100

    Creación de actividad14

---
+++ ejercicio14.js
@@ -0,0 +1,9 @@
+// Un array de nombre diasSemana para contener 7 elementos. 
+// Luego rellena el array con los nombres de los días comenzando por Lunes hasta Domingo.
+
+var diasSemana;
+console.log(diasSemana)
+diasSemana=['Lunes', 'Martes','Miércoles','Jueves','Viernes', 'Sábado','Domingo'];
+
+document.write(diasSemana+"<br>");
+document.write(diasSemana[3]);
