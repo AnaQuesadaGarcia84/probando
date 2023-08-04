ee1272b3ac186300ccdbbbeb9decb0b57d03092a
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-08-04 20:04:05 +0100

    Cambios en el ejercicio 2

--- ExamenFinalJS2/jquery.js
+++ ExamenFinalJS2/jquery.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-    $("#profesor").click(function(){
-        $(".cuerpo").css("background-image", "url('media/fondoprofesor.jpg')");
-    })
     $("#alumno").click(function(){
-        $(".cuerpo").css("background-image", "url('media/fondoalumnos.jpg')");
+        $(".contenido").css("background-image", "url('media/fondoalumnos.jpg')");
+        $(".h1").text('Bienvenido alumno');
+        $(".texto").text('Si quieres ponerte en contacto con nosotros pulsa el botón de información')
     })
+
+    //Ventana emergente
+    //Abrirla
+    $("#profesor").click(function(){
+        $('.profesorEmergente').addClass('active');
+    });
+
+    //Cerrarla
+    $(".cerrar").click(function(){
+        $('.profesorEmergente').removeClass('active');
+    });
+
+    //Cambiamos estilos para la versión nocturna
+
 })//Cierre del document ready
